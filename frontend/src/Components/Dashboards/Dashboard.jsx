8b47355c166503ea75eab1a2267651dcfc5e317a
Saadaqmacalin <saadaqmohavic20@gmail.com>
2025-11-06 17:39:10 +0300

    made changes on the dashboard

--- frontend/src/Components/Dashboards/Dashboard.jsx
+++ frontend/src/Components/Dashboards/Dashboard.jsx
@@ -2,25 +2,60 @@ import React from "react";
 
 const Dashboard = () => {
   return (
-    <div className="flex ml-30 mt-8 ">
+    <div className="p-4">
+      <h2 className="text-2xl font-extrabold ml-4">Dashboard Section</h2>
 
-      <div className="bg-indigo-500 text-2xl text-center  w-40 h-40 rounded-2xl">
-        <h2 className="p-15 text-white">0</h2>
-        
+      <div className="flex mt-8 mb-10 gap-10 flex-wrap">
+        <div>
+          <button className="text-2xl font-bold">Add User</button>
+          <div className="h-40 w-40 mt-4 text-center text-black shadow-xl p-4 rounded-2xl border">
+            <h2 className="text-xl font-extralight">Total Users</h2>
+            <h2 className="pt-6 text-3xl font-extrabold text-indigo-600">
+              234
+            </h2>
+          </div>
+        </div>
+
+        <div>
+          <button className="text-2xl font-bold">Add User</button>
+          <div className="h-40 w-40 mt-4 text-center text-black shadow-xl p-4 rounded-2xl border">
+            <h2 className="text-xl font-extralight">Total Users</h2>
+            <h2 className="pt-6 text-3xl font-extrabold text-indigo-600">
+              234
+            </h2>
+          </div>
+        </div>
+
+        <div>
+          <button className="text-2xl font-bold">Add User</button>
+          <div className="h-40 w-40 mt-4 text-center text-black shadow-xl p-4 rounded-2xl border">
+            <h2 className="text-xl font-extralight">Total Users</h2>
+            <h2 className="pt-6 text-3xl font-extrabold text-indigo-600">
+              234
+            </h2>
+          </div>
+        </div>
+
+        <div>
+          <button className="text-2xl font-bold">Add User</button>
+          <div className="h-40 w-40 mt-4 text-center text-black shadow-xl p-4 rounded-2xl border">
+            <h2 className="text-xl font-extralight">Total Users</h2>
+            <h2 className="pt-6 text-3xl font-extrabold text-indigo-600">
+              234
+            </h2>
+          </div>
+        </div>
+
+        <div>
+          <button className="text-2xl font-bold">Add User</button>
+          <div className="h-40 w-40 mt-4 text-center text-black shadow-xl p-4 rounded-2xl border">
+            <h2 className="text-xl font-extralight">Total Users</h2>
+            <h2 className="pt-6 text-3xl font-extrabold text-indigo-600">
+              234
+            </h2>
+          </div>
+        </div>
       </div>
-      <div className="bg-indigo-500 text-2xl text-center ml-12 w-40 h-40 rounded-2xl">
-        <h2 className="p-15 text-white">0</h2>
-      </div>
-      <div className="bg-indigo-500 text-2xl text-center ml-12 w-40 h-40 rounded-2xl">
-        <h2 className="p-15 text-white">0</h2>
-      </div>
-      <div className="bg-indigo-500 text-2xl text-center ml-12 w-40 h-40 rounded-2xl">
-        <h2 className="p-15 text-white">0</h2>
-      </div>
-      <div className="bg-indigo-500 text-2xl text-center ml-12 w-40 h-40 rounded-2xl">
-        <h2 className="p-15 text-white">0</h2>
-      </div>
-      
     </div>
   );
 };
